b0ba5eeec45f94eb8fb9b46563a39822f8aec389
PraveenSiva77 <praveensiva0820@gmail.com>
2024-09-13 00:24:38 +0530

    Update v1.9

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Praveenkumar S
 
 ## Update Log
-  - Latest Update v1.8
+  - Latest Update v1.9
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/praveenkumar-s'>
       <ThemeProvider>
         <div className='dark:bg-gray-900'>
           <App />
