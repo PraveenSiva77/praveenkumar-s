bb16021056f00300cfe1ce6d0671348245358f10
PraveenSiva77 <praveensiva0820@gmail.com>
2025-06-30 18:44:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
+import { Component } from "react";
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import Toaster from './components/Toaster'
 import './App.css'
-import { HelmetProvider } from 'react-helmet-async'
 
-function App() {
-  return (
-    <div className='bg-black text-white min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black font-sans antialiased overflow-x-hidden'>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </HelmetProvider>
-    </div>
-  )
+class App extends Component {
+    render() {
+        return (
+            <div className='bg-black text-white min-h-screen bg-gradient-to-tl from-black via-zinc-600/20 to-black font-sans antialiased overflow-x-hidden'>
+                <RouterProvider router={router} />
+                <Toaster />
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
